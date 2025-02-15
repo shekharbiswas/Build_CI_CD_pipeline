4b07193f1776458e82d4c704cedfb828b2ed97c3
Shekhar Biswas <shekhar07.nitdgp@gmail.com>
2025-02-15 08:14:43 +0100

    updated with joblib creation

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-from sklearn.externals import joblib
+import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
@@ -20,43 +20,20 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h2> Sklearn Prediction Home APP - REST API</h2>"    
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
 def predict():
-    """Performs an sklearn prediction
-
-    input looks like:
-            {
-    "CHAS":{
-      "0":0
-    },
-    "RM":{
-      "0":6.575
-    },
-    "TAX":{
-      "0":296.0
-    },
-    "PTRATIO":{
-       "0":15.3
-    },
-    "B":{
-       "0":396.9
-    },
-    "LSTAT":{
-       "0":4.98
-    }
-
-    result looks like:
-    { "prediction": [ 20.35373177134412 ] }
-
-    """
-
+    # Performs an sklearn prediction
     try:
-        clf = joblib.load("./Housing_price_model/boston_housing_prediction.joblib")
-    except:
+        # Load pretrained model as clf. Try any one model. 
+        clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
+        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
+        # clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+        
+    except Exception:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
 
